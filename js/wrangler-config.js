37d0dc210a51793a7b9932d82bf74418754de2dc
Adam Burdett <burdettadam@gmail.com>
2016-08-18 00:00:10 -0600

    use my app key

--- js/wrangler-config.js
+++ js/wrangler-config.js
@@ -1,6 +1,6 @@
-wrangler.clientKey = "0F03372E-1078-11E5-BCFA-5FDDE71C24E1";
+wrangler.clientKey = "76FEFB48-6508-11E6-9312-37E6E71C24E1";
 wrangler.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
-wrangler.callbackURL = "http://kre.github.io/devtools/code.html";
+wrangler.callbackURL = "https://burdettadam.github.io/devtools/code.html";
 wrangler.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
 wrangler.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
 wrangler.eventPath = 'sky/event';
